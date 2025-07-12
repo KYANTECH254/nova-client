c8b08176c531eaa3f845216da92ae80fbf591224
Kyan254 Dev <danielkyanuiofficial@gmail.com>
2025-07-12 18:25:33 +0000

    Initial commit

--- ssh.sh
+++ ssh.sh
@@ -7,3 +7,17 @@ ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
 
 ssh -T git@github.com
+
+git init 
+
+git add .
+
+git remote add origin git@github.com:KYANTECH254/nova-client.git
+
+git remote -v
+
+git branch -M main
+
+git commit -m "Initial commit"
+
+git push -u origin main
