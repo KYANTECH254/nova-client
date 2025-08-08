d4200bbd7520bb3360d0c21a694c912e818cc23c
NETFUNDI WIFI <netfundiwifi@gmail.com>
2025-08-08 20:16:54 +0000

    initial commit

--- components/Users.tsx
+++ components/Users.tsx
@@ -4,7 +4,7 @@ import { cache, useEffect, useState } from "react";
 import Table from "./Table";
 import { useAdminAuth } from "@/contexts/AdminSessionProvider";
 import { toast } from "sonner";
-import { Trash2 } from "lucide-react";
+import { Edit, Trash2 } from "lucide-react";
 
 type User = {
   id: string;
@@ -12,6 +12,7 @@ type User = {
   username: string;
   code: string;
   status: "active" | "inactive" | "expired";
+  active: string;
   package: string;
   updatedAt: string;
   createdAt: string;
@@ -22,6 +23,11 @@ export default function Users() {
   const [searchValue, setSearchValue] = useState("");
   const { isAuthenticated, adminUser, token } = useAdminAuth();
   const [isDeleting, setIsDeleting] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<any>();
+  const [phone, setphone] = useState("")
+  const [username, setusername] = useState("")
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchUsers = cache(async () => {
@@ -84,6 +90,15 @@ export default function Users() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+  };
+
+
+  const handleEdit = (usr: User) => {
+    const matchingUser = filteredUsers.find(user => user.id === usr.id);
+    setSelectedUser(matchingUser || "");
+    setusername(usr.username);
+    setphone(usr.phone);
+    setShowModal(true);
   };
 
   const handleDelete = cache(async (user: User) => {
@@ -164,7 +179,7 @@ export default function Users() {
       header: "Code",
       accessor: "username",
     },
-        {
+    {
       header: "Package",
       accessor: "package",
     },
@@ -185,6 +200,20 @@ export default function Users() {
       ),
     },
     {
+      header: "Active",
+      accessor: "active",
+      render: (value: string) => (
+        <span
+          className={`px-2 py-1 rounded-full text-xs ${value === "Online"
+            ? "bg-green-900/20 text-green-800"
+            : value === "Offline" && "bg-red-900/20 text-red-800"
+            }`}
+        >
+          {value.charAt(0).toUpperCase() + value.slice(1)}
+        </span>
+      ),
+    },
+    {
       header: "Last Login",
       accessor: "updatedAt",
       render: (value: string) => new Date(value).toLocaleString(),
@@ -194,6 +223,13 @@ export default function Users() {
       accessor: "id",
       render: (value: string, row: User) => (
         <div className="flex space-x-2">
+          <button
+            onClick={() => handleEdit(row)}
+            className="text-blue-600 hover:text-blue-800"
+            aria-label="Edit user"
+          >
+            <Edit size={18} />
+          </button>
           <button
             onClick={() => handleDelete(row)}
             disabled={isDeleting}
@@ -207,16 +243,109 @@ export default function Users() {
     },
   ];
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    setIsEditing(true);
+    e.preventDefault();
+    const userData = {
+      id: selectedUser.id,
+      username: username,
+      phone: phone
+    };
+
+    try {
+      const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/mkt/updateUser`;
+      const method = "POST";
+
+      const response = await fetch(url, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: token, userData }),
+      });
+      const res = await response.json();
+      if (res.success) {
+        toast.success(res.message);
+      } else {
+        toast.error(res.message);
+      }
+    } catch (error) {
+      console.log("Error submitting user:", error);
+      toast.error("Failed to submit user");
+    } finally {
+      setIsEditing(false);
+    }
+    setShowModal(false);
+  };
+
   return (
-    <Table
-      data={filteredUsers}
-      columns={columns}
-      onExport={handleExport}
-      title="Users"
-      showSearch={true}
-      searchValue={searchValue}
-      onSearchChange={setSearchValue}
-      onDeleteSelected={handleDeleteSelectedUsers}
-    />
+    <div className="p-4">
+      <Table
+        data={filteredUsers}
+        columns={columns}
+        onExport={handleExport}
+        title="Users"
+        showSearch={true}
+        searchValue={searchValue}
+        onSearchChange={setSearchValue}
+        onDeleteSelected={handleDeleteSelectedUsers}
+      />
+
+
+      {showModal && (
+        <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4 z-50">
+          <div className="flex flex-col bg-gray-900 border border-gray-700 text-gray-100 rounded-lg shadow-2xl p-6 w-full max-w-md max-h-full overflow-y-auto space-y-6">
+            <h2 className="text-xl font-bold mb-4">
+              Edit User
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="space-y-4">
+                <div>
+                  <label className="block text-sm font-medium mb-1">Code</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={username || selectedUser?.username || ""}
+                    onChange={(e) => setusername(e.target.value)}
+                    className="w-full px-3 py-2 border rounded-md bg-black text-gray-300"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium mb-1">Phone</label>
+                  <input
+                    type="text"
+                    name="address"
+                    value={phone || selectedUser?.phone || ""}
+                    onChange={(e) => setphone(e.target.value)}
+                    className="w-full px-3 py-2 border rounded-md bg-black text-gray-300"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-end space-x-2 mt-6">
+                <button
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                  className="px-4 py-2 rounded-md bg-red-600 hover:bg-red-700"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+                  disabled={isEditing}
+                >
+                  {isEditing ? "Updating..." : "Update User"}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      )
+      }
+    </div>
   );
 }
